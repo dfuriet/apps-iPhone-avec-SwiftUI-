0251af3973c1ee1b1fbc0244a4f7058f82930c6d
Damien FURIET <damien@furiet.net>
2022-06-13 21:40:29 +0200

    Initial Commit

---
+++ Clicker/ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickerApp.swift
+//  Clicker
+//
+//  Created by Damien FURIET on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
