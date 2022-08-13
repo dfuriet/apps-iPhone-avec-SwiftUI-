312b9b03c6bf53823f2ee7ba9b9f5ee3636b6f45
Damien FURIET <damien@furiet.net>
2022-08-13 10:27:43 +0200

    1H pour créer une jeu iPhone avec SwiftUI

--- Clicker/ContentView.swift
+++ Clicker/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var score = 0
+    @State var bestScore = 0
+    @State var gameIsInProgress = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                if score >= bestScore && score != 0 {
+                    Image(systemName: "flame")
+                }
+                Text("Score = \(score)")
+                    .padding()
+            }.font(.title)
+            if gameIsInProgress == true {
+                Image(systemName: "plus.square")
+                    .font(.title)
+                    .onTapGesture {
+                        score = score + 1
+                    }
+            }
+            Spacer()
+            if gameIsInProgress == false {
+                Button("Nouvelle partie") {
+                    score = 0
+                    gameIsInProgress = true
+                    Timer.scheduledTimer(withTimeInterval: 10.0, repeats: false) { (_) in
+                        gameIsInProgress = false
+                        if score > bestScore {
+                            bestScore = score
+                        }
+                    }
+                }.padding()
+            }
+        }
     }
 }
 
